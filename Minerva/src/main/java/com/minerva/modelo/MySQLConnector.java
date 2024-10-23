fb6d71e056be1511b42fb53e4eb954770f3fac88
luis13211 <165101448+luis13211@users.noreply.github.com>
2024-10-22 20:04:39 -0500

    Metodos agregados para MySQLConecor

--- Minerva/src/main/java/com/minerva/modelo/MySQLConnector.java
+++ Minerva/src/main/java/com/minerva/modelo/MySQLConnector.java
@@ -2,14 +2,18 @@ package com.minerva.modelo;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
  * @author L
  */
 public class MySQLConnector {
-    private Connection conectar = null;
-    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private Connection conexion = null;
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String USUARIO = "root";
     private static final String CONTRASEÑA = "#";
     private static final String BD = "Apolo";
@@ -18,34 +22,64 @@ public class MySQLConnector {
     
     private static final String URL = "jdbc:mysql://" + IP +":" + PUERTO + "/ " + BD;
 
+    private PreparedStatement preparedStatement = null;
+    private Statement statement = null;
+    
+    private ResultSet resultSet = null; 
+    
     public MySQLConnector() {
     }
  
-    public Connection establecerConexion() {
+    private void establecerConexion() {
         try {
+            // SE CARGA EL DRIVER 
             Class.forName(DRIVER);
-            conectar = DriverManager.getConnection(URL, USUARIO, CONTRASEÑA);
+            // SE ESTABLECE LA CONEXION
+            conexion = DriverManager.getConnection(URL, USUARIO, CONTRASEÑA);  
+            
             System.out.println("CONEXION CON LA BASE DE DATOS " + BD + " ESTABLECIDA");
+        } catch (SQLException e) {
+            System.out.println("CONEXION CON LA BASE DE DATOS " + BD + " RECHAZADA");
+            System.out.println("ERROR: " + e.toString());
         } catch (Exception e) {
-            System.out.println("CONEXION CON LA BASE DE DATOS " + BD + " FUERA DE LINEA");
             System.out.println("ERROR: " + e.toString());
         }
-        
-        return conectar;
     }
     
-    public Connection cerrarConexion() {
+    private void cerrarConexion() {
         try {
             // SI CONECTAR ES DIFERENTE DE NULO Y DIFERENTE DE ABIERTO SE CIERRA LA CONEXION
-            if (conectar != null && !conectar.isClosed()) {
-                conectar.close();
+            if (conexion != null && !conexion.isClosed()) {
+                conexion.close();
                 System.out.println("CONEXION CON LA BASE DE DATOS " + BD + " CERRADA");
+            } else {
+                System.out.println("LA CONEXION CON LA BASE DE DATOS " + BD + " YA ESTABA CERRADA");
             }
-        } catch (Exception e) {
+        } catch (SQLException e) {
             System.out.println("CONEXION CON LA BASE DE DATOS " + BD + " NO PUDO SER CERRADA");
-            System.out.println("ERROR: " +  e.toString());
-        }
+            System.out.println("ERROR: " + e.toString());
+        }       
+    }
+    
+    public ResultSet consultaSQL(String query) throws SQLException {
+        establecerConexion();
+        statement = conexion.createStatement();        
+        resultSet = statement.executeQuery(query);
         
-        return conectar;
+        cerrarConexion();
+        return resultSet;
+    }
+    
+    // PARA EVITAR INYECCIONES SQL PERO NO SE SI DEBERIA IMPLEMENTARLO PARA ESTE PROYECTO XD
+    // LO DEJO AQUI POR LAS DUDAS
+    public ResultSet consultaSQLDinamica(String query) throws SQLException {
+        establecerConexion();
+
+        preparedStatement = conexion.prepareStatement(query);
+        preparedStatement.setInt(1, 0);
+        resultSet = preparedStatement.executeQuery();
+        
+        cerrarConexion();
+        return resultSet;
     }
 }
